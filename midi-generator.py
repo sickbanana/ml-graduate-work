4b6ef045cc9730553a2a767be27bab313cd04c61
sickbanana <serzh.karasev.1999@mail.ru>
2024-01-23 05:38:27 +0300

    vectorize midi

--- midi-generator.py
+++ midi-generator.py
@@ -1,15 +1,52 @@
+import collections
 import pathlib
 import glob
+
+import numpy as np
 import tensorflow as tf
 
-data_dir = pathlib.Path('data/maestro-v2.0.0')
-if not data_dir.exists():
-    tf.keras.utils.get_file(
-      'maestro-v2.0.0-midi.zip',
-      origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
-      extract=True,
-      cache_dir='.', cache_subdir='data',
-    )
 
-filenames = glob.glob(str(data_dir / '**/*.mid*'))
-print('Number of files:', len(filenames))
+def midi_to_notes(midi_file: str) -> pd.DataFrame:
+    pm = pretty_midi.PrettyMIDI(midi_file)
+    instrument = pm.instruments[0]
+    notes = collections.defaultdict(list)
+
+    sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
+    prev_start = sorted_notes[0].start
+
+    for note in sorted_notes:
+        start = note.start
+        end = note.end
+        notes['pitch'].append(note.pitch)
+        notes['start'].append(start)
+        notes['end'].append(end)
+        notes['step'].append(start - prev_start)
+        notes['duration'].append(end - start)
+        prev_start = start
+
+    return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+
+
+if __name__ == '__main__':
+    data_dir = pathlib.Path('data/maestro-v2.0.0')
+    if not data_dir.exists():
+        tf.keras.utils.get_file(
+            'maestro-v2.0.0-midi.zip',
+            origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
+            extract=True,
+            cache_dir='.', cache_subdir='data',
+        )
+
+    filenames = glob.glob(str(data_dir / '**/*.mid*'))
+    print('Number of files:', len(filenames))
+
+    num_files = 100
+    all_notes = []
+    for f in filenames[start:num_files]:
+        notes = midi_to_notes(f)
+        all_notes.append(notes)
+    all_notes = pd.concat(all_notes)
+
+    key_order = ['pitch', 'step', 'duration']
+    train_notes = np.stack([all_notes[key] for key in key_order], axis=1)
+    X_train = tf.data.Dataset.from_tensor_slices(train_notes)
