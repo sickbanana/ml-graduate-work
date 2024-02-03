c2bfd212e7e038ba25c53267f1d08c9f4a4ce393
sickbanana <serzh.karasev.1999@mail.ru>
2024-02-03 11:23:50 +0300

    fix libs

--- rnn_model_v2.py
+++ rnn_model_v2.py
@@ -1,9 +1,18 @@
 import collections
+import pathlib
+import glob
 import pretty_midi
 
+import matplotlib
+matplotlib.use('TkAgg')
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
+import pandas as pd
+
 
 def midi_to_notes(midi_file: str) -> pd.DataFrame:
     pm = pretty_midi.PrettyMIDI(midi_file)
@@ -26,7 +35,28 @@ def midi_to_notes(midi_file: str) -> pd.DataFrame:
     return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
 
 
+
+
 if __name__ == '__main__':
+    data_dir = pathlib.Path('data/maestro-v2.0.0')
+    if not data_dir.exists():
+        tf.keras.utils.get_file(
+            'maestro-v2.0.0-midi.zip',
+            origin='https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip',
+            extract=True,
+            cache_dir='.', cache_subdir='data',
+        )
+
+    filenames = glob.glob(str(data_dir / '**/*.mid*'))
+    print('Number of files:', len(filenames))
+
+    num_files = 50
+    all_notes = []
+    for f in filenames[:num_files]:
+        notes = midi_to_notes(f)
+        all_notes.append(notes)
+
+
     seq_length = 32
     step = 10
     vocab_size = 128
